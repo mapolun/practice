88da70490f91a2fadd27643c19f17ed56d1a2cb5
wtt <1364576479@qq.com>
2021-12-18 17:11:56 +0800

    php两数之和

--- php/两数之和.php
+++ php/两数之和.php
@@ -3,3 +3,53 @@
  * Author mapo
  * Date   2021/12/18
  */
+
+/*
+    给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+    你可以按任意顺序返回答案。
+*/
+
+//单个答案
+function sum2(array $nums, int $target) : array
+{
+    for ($i = 0; $i < count($nums);$i++) {
+        // 计算剩下的数
+        $residue = $target - $nums[$i];
+        // 匹配的index，有则返回index， 无则返回false
+        $match_index = array_search($residue, $nums);
+        if ($match_index !== false && $match_index != $i) {
+            return array($i, $match_index);
+        }
+    }
+    return [];
+}
+
+$res = sum2([1,2,3,4,5],5);
+var_dump($res);
+
+//多个答案
+function sum_more(array $nums, int $target) : array
+{
+    $res = [];
+    for ($i = 0; $i < count($nums);$i++) {
+        // 计算剩下的数
+        $residue = $target - $nums[$i];
+        // 匹配的index，有则返回index， 无则返回false
+        $match_index = array_search($residue, $nums);
+        if ($match_index !== false && $match_index != $i) {
+            $key = min($i, $match_index).'_'.max($i, $match_index);
+            if (!isset($res[$key])) {
+                $res[$key] = array_map(function ($value){
+                    return intval($value);
+                }, explode('_', $key));
+            }
+        }
+    }
+    return array_values($res);
+}
+
+$res = sum_more([1,2,3,4,5],5);
+var_dump($res);
